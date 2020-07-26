c3e00d96ab0452be38ee81267ebdd418858889ff
Callie Crownover <crownovercallie@gmail.com>
2020-07-26 08:36:06 -0400

    worked on front end designs a bit more and fixed the background image issue

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 //IMPORT THE MODEL (burger.js) to use its database function
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
+// Create all the routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var handlebarObject = {
